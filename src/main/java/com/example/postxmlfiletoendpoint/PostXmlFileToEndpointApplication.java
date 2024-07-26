f5b5e4a3533a4bd7f45f65090d206235244bd9ae
Peter Albertsson <psweden@users.noreply.github.com>
2024-07-26 16:02:27 +0200

    First commit files from new project will be commited to GitHub

---
+++ src/main/java/com/example/postxmlfiletoendpoint/PostXmlFileToEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.postxmlfiletoendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostXmlFileToEndpointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostXmlFileToEndpointApplication.class, args);
+    }
+
+}
