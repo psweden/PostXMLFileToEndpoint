93b29b28fd69f0095e9a88a3918cb254e031949c
Peter Albertsson <psweden@users.noreply.github.com>
2024-07-26 16:02:27 +0200

    First commit files from new project will be commited to GitHub

---
+++ src/test/java/com/example/postxmlfiletoendpoint/PostXmlFileToEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postxmlfiletoendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostXmlFileToEndpointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
